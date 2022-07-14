4fe4f92d97a92e4bf47373429d0aeaebde3698e5
Tomas Pulenta <tpulenta@gmail.com>
2022-07-14 20:15:28 -0300

    primer commit sacando typescript

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return(
+       <div className="navbar1">
+          <div className="navbar2">
+             <a className="navbar3" href="/">Home</a>
+             <a className="navbar3" href="/categories">categories</a>
+           </div>
+           <div className="navbar3">
+             <a className="navbar3" href="/about">About</a>
+             <a className="navbar3" href="/contact">Contact</a>
+             <a className="navbar3" href="/privacy-policy">Privacy Policy</a>
+            </div>
+         </div>
+      )
+  }
