a7a2794100bee54da52bf6a069e5334ebc585d8b
Tomas Pulenta <tpulenta@hexacta.com>
2022-07-14 16:00:44 -0300

    initial commit nextjs

---
+++ constants/constants.ts
@@ -0,0 +1,11 @@
+export const Constants = {
+    Colors: {
+        // brandGreenColor: '#03960A',
+        brandGreenColor: '#4caf50',
+        brandBlueColor: '#1976d2',
+        cancelColor: '#ad0000',
+        white: '#fff',
+        grey: '#808080',
+        backgroundGrey: '#f0f0f0'
+    }
+} 
